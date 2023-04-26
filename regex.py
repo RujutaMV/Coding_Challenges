14dc7369b2b40613e6e01c84c81794128e8dd815
Rujuta Vaidya <118907087+RujutaMV@users.noreply.github.com>
2023-04-26 16:01:07 +0200

    Create regex.py

---
+++ regex.py
@@ -0,0 +1,9 @@
+# 8. Find all task codes in the text with using regular expression
+
+import re
+q8_input = '''Working on QA-456 task.
+Trying to finish soon. Then will continue
+with GH-12.'''
+q8_output = re.findall(r'[A-Z]+-[0-9]*', q8_input)  # change it
+print(f'{q8_output=}')
+print(f'\n{"=" * 10}\n')
